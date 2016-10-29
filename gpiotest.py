616f9eec66ea11fa33f46afe4388eb7e7caa8841
bkbilly <bkbilly@hotmail.com>
2016-10-29 22:20:49 +0000

    fixed multiple sensors

--- gpiotest.py
+++ gpiotest.py
@@ -7,9 +7,11 @@ import threading
 class DoorSensor():
     """docstring for DoorSensor"""
     def __init__(self, jsonfile):
+        GPIO.setmode(GPIO.BCM)
         self.jsonfile = jsonfile
         self.setAlert = False
         self.kill_now = False
+        self.enabledPins = []
 
         # Stop execution on exit
         signal.signal(signal.SIGINT, self.exit_gracefully)
@@ -25,24 +27,26 @@ class DoorSensor():
             time.sleep(1)
 
     def myCallBack(self, inputPin):
+        print "------------"
         foundInputPin = False
         self.settings = self.readSettings(self.jsonfile)
         self.setAlert = False
         for sensor in self.settings["sensors"]:
             if sensor["active"] == True:
                 foundInputPin = True
-                GPIO.setmode(GPIO.BCM)
-                GPIO.setup(sensor["pin"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-                GPIO.remove_event_detect(inputPin)
-                GPIO.add_event_detect(sensor["pin"], GPIO.BOTH, callback=self.myCallBack)
+                if sensor["pin"] not in self.enabledPins:
+                    self.enabledPins.append(sensor["pin"])
+                    GPIO.setup(sensor["pin"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+                    GPIO.remove_event_detect(sensor["pin"])
+                    GPIO.add_event_detect(sensor["pin"], GPIO.BOTH, callback=self.myCallBack)
                 if GPIO.input(sensor["pin"]) == 1:
                     print sensor["name"]
                     self.setAlert = True
         if foundInputPin == False:
             GPIO.remove_event_detect(inputPin)
+            self.enabledPins.remove(inputPin)
         if self.setAlert == False:
             print "No alarm!"
-
 
     def exit_gracefully(self, signum, frame):
         self.kill_now = True
