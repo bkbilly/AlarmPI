2bcfb6f576ec0a5cdf15f8d0c9aef362af3c1a5e
bkbilly <bkbilly@hotmail.com>
2019-10-10 16:41:58 +0300

    remove otp

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 REQUIRES = [
-    'Flask==1.1.1',
-    'Flask-SocketIO==4.2.1',
-    'Flask-Login==0.4.1',
-    'requests==2.22.0',
-    'pytz==2019.2',
-    'paho-mqtt==1.4.0',
-    'RPi.GPIO==0.7.0',
+    'Flask>=1.1.1',
+    'Flask-SocketIO>=4.2.1',
+    'Flask-Login>=0.4.1',
+    'requests>=2.22.0',
+    'pytz>=2019.2',
+    'paho-mqtt>=1.4.0',
+    'RPi.GPIO>=0.7.0',
 ]
 
 
 setup(
     name='AlarmPI',
-    version='4.0',
+    version='4.1',
     description='Home Security System',
     author='bkbilly',
     author_email='bkbilly@hotmail.com',
